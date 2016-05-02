ee8ef3c11bd145f1b8d2971c191a47cba2ff3ded
KoBe <kobe@kobedemacbook-pro.local>
2016-05-02 11:10:22 +0800

    890

---
+++ 890/890/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  890
+//
+//  Created by KoBe on 16/5/2.
+//  Copyright © 2016年 KoBe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
